f3f2ed75a7b447f0a970225b3374ab01afcb520a
Kevin Ialo Arroyo Apolinario <90014815+kevin999666@users.noreply.github.com>
2024-04-02 04:29:10 +0000

    3er commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
+import { SlickCarouselModule } from 'ngx-slick-carousel';
+
 import { AppComponent } from './app.component';
-import { CarouselModule } from 'ngx-bootstrap/carousel';
-import { PaginaPrincipalComponent } from './components/pagina-principal/pagina-principal.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { PaginaPrincipalComponent } from './components/pagina-principal/pagina-principal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PaginaPrincipalComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    PaginaPrincipalComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CarouselModule.forRoot()
+    SlickCarouselModule
   ],
   providers: [],
   bootstrap: [AppComponent]
