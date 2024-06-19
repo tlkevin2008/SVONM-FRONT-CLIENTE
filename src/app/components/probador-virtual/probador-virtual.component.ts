a491bc78bacf3d4303d9f61e809c1fe7ae10bd14
carlosrivasesp <202110052@urp.edu.pe>
2024-06-19 12:53:46 -0500

    Intento de Probador Virtual: Funcionalidad de Volver a tomar captura y seleccionar Producto

--- src/app/components/probador-virtual/probador-virtual.component.ts
+++ src/app/components/probador-virtual/probador-virtual.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 import { Subject } from 'rxjs';
 import { ImageUpService } from 'src/app/services/imageUp.service';
-import { Image } from 'src/app/models/image';
+import { Imagen } from 'src/app/models/image';
+import { Catalogo } from 'src/app/models/catalogo';
+import { CatalogoService } from 'src/app/services/catalogo.service';
 
 @Component({
   selector: 'app-probador-virtual',
@@ -10,32 +12,56 @@ import { Image } from 'src/app/models/image';
 })
 export class ProbadorVirtualComponent {
 
-  // Variable para almacenar la imagen capturada
-  capturedImage: string | any;
-
-  // Variable para el gatillo de la captura
+  listCatalogos: Catalogo[] = [];
+  capturedImage: Imagen | undefined;
+  selectedProductImage: string | undefined;
+  showCamera = true;
   private _trigger = new Subject<void>();
   public readonly captureTrigger = this._trigger.asObservable();
 
-  constructor(private imageUpService: ImageUpService) { }
+  constructor(private imageUpService: ImageUpService, private _catalogoService: CatalogoService) { }
 
-  // Método para capturar una imagen
   capture(): void {
     this._trigger.next();
   }
 
+  retake(): void {
+    this.capturedImage = undefined;
+    this.showCamera = true;
+  }
+
+  onImageCapture(event: any): void {
+    this.capturedImage = new Imagen(event.imageAsDataUrl);
+    this.showCamera = false;
+  }
+
   uploadImage(): void {
     if (this.capturedImage) {
-      const image: Image = { data: this.capturedImage };
-      this.imageUpService.uploadImage(image).subscribe(
+      this.imageUpService.uploadImage(this.capturedImage).subscribe(
         response => {
-          console.log('Image uploaded successfully', response);
+          console.log('Imagen subida con éxito', response);
         },
         error => {
-          console.error('Error uploading image', error);
+          console.error('Error al subir la imagen', error);
         }
       );
     }
   }
 
+  onProductSelect(productImageUrl: string): void {
+    this.selectedProductImage = productImageUrl;
+  }
+
+  ngOnInit(): void {
+    this.obtenerCatalogos();
+  }
+
+  obtenerCatalogos(){
+    this._catalogoService.getCatalogos().subscribe(data => {
+      console.log(data);
+      this.listCatalogos = data;
+    }, error => {
+      console.log(error);
+    })
+  }
 }
