1ba18346bb283c31732aa6cbc6599f1f7fbe18bc
KevinArroyo <kevinitalo.arroyo@urp.edu.pe>
2024-07-02 11:37:41 -0500

    Tarea de los tp2 completa

--- src/app/components/contactos/contactos.component.ts
+++ src/app/components/contactos/contactos.component.ts
@@ -10,41 +10,61 @@ import { ContactoService } from 'src/app/services/contacto.service';
   templateUrl: './contactos.component.html',
   styleUrls: ['./contactos.component.css']
 })
-export class ContactosComponent implements OnInit{
+export class ContactosComponent implements OnInit {
   contactoForm: FormGroup;
 
-  constructor(private fb: FormBuilder,
-              private router: Router,
-              private toastr: ToastrService,
-              private _contactoService: ContactoService) {
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private toastr: ToastrService,
+    private _contactoService: ContactoService
+  ) {
     this.contactoForm = this.fb.group({
-      nombre: ['', Validators.required],
-      telefono: ['', Validators.required],
-      email: ['', Validators.required],
-      mensaje: ['', Validators.required],
-    })
-   }
-
-  ngOnInit(): void {
-      
+      nombre: ['', [
+        Validators.required,
+        Validators.pattern(/^(?!\s)(?!.*\s{2})(?!.*[^a-zA-Z\s]).{1,20}$/)
+      ]],
+      telefono: ['', [
+        Validators.required,
+        Validators.pattern(/^[0-9]{7,9}$/)
+      ]],
+      email: ['', [
+        Validators.required,
+        Validators.pattern(/^\S+@\S+\.\S{1,19}$/)
+      ]],
+      mensaje: ['', [
+        Validators.required,
+        Validators.maxLength(50)
+      ]],
+    });
   }
 
-  agregarContacto(){
+  ngOnInit(): void {}
 
-    const CONTACTO: Contacto ={
+  agregarContacto() {
+    console.log(this.contactoForm);
+
+    if (this.contactoForm.invalid) {
+      return;
+    }
+
+    const CONTACTO: Contacto = {
       nombre: this.contactoForm.get('nombre')?.value,
       telefono: this.contactoForm.get('telefono')?.value,
       email: this.contactoForm.get('email')?.value,
       mensaje: this.contactoForm.get('mensaje')?.value,
-    }
+    };
 
     console.log(CONTACTO);
-    this._contactoService.guardarContacto(CONTACTO).subscribe(data => {
-      this.toastr.success('El contacto fue realizado con exito!', 'Contacto Enviado!');
-      this.router.navigate(['/'])
-    }, error => {
-      console.log(error);
-      this.contactoForm.reset();
-    })
+    this._contactoService.guardarContacto(CONTACTO).subscribe(
+      data => {
+        this.toastr.success('El contacto fue realizado con éxito!', 'Contacto Enviado!');
+        this.router.navigate(['/']);
+      },
+      error => {
+        console.log(error);
+        this.contactoForm.reset();
+      }
+    );
   }
 }
